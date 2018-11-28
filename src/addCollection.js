607f327f4e2204b15663adfc15be1790cec737e5
lugDroid <diazbastos@gmail.com>
2018-11-28 09:29:11 +0100

    Volume search tests

--- src/addCollection.js
+++ src/addCollection.js
@@ -4,15 +4,24 @@ const proxy = 'https://allorigins.me/get?method=raw&url='
 const baseURL = 'http://comicvine.gamespot.com/api'
 const apiKey = '8cd3ed9e59e0d9a58377048e00674d893b7a2450'
 
-fetch(baseURL + '/volumes/?api_key=' + apiKey + '&format=json&filter=name:mister%20miracle', {mode: 'no-cors'})
+// TO-DO: this value should come from an input box
+let searchText = 'mister%20miracle'
+
+// search volumes
+fetch(baseURL + '/volumes/?api_key=' + apiKey + '&format=json&filter=name:' + searchText,
+{mode: 'no-cors'})
   .then(response => response.json())
   .then(json => {
-    json.results.forEach(element => {
-      console.log(element.start_year + ' - ' + element.name)
+    json.results.forEach(volume => {
+      console.log(volume.start_year + ' - ' + volume.name + ' ' + volume.deck)
     })
-    const volumeName = json.results[7].name.replace(' ', '%20')
-    console.log(volumeName)
-    fetch(baseURL + '/issues/?api_key=' + apiKey + '&format=json&filter=volume:' + volumeName)
+    // TO-DO: at this point the user will have to select one of the returned volumes
+
+    // get selected volume id
+    const volumeID = json.results[7].id
+
+    // get all issues for a selected volume
+    fetch(baseURL + '/issues/?api_key=' + apiKey + '&format=json&filter=volume:' + volumeID)
       .then(res => res.json())
       .then(json => console.log(json))
   })
