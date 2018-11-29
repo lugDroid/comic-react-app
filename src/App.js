8cd19db924af70c8f6888c5178a4433b15116376
lugDroid <diazbastos@gmail.com>
2018-11-29 18:18:02 +0100

    React components started

--- src/App.js
+++ src/App.js
@@ -1,35 +1,114 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  componentDidMount() {
-    const proxy = 'https://allorigins.me/get?method=raw&url='
-    const baseURL = 'http://comicvine.gamespot.com/api'
-    const apiKey = '8cd3ed9e59e0d9a58377048e00674d893b7a2450'
+const misterMiracle = {
+  title: 'Mister Miracle',
+  totalIssues: 12,
+  ownedIssues: 12,
+  status: 'Finished',
+  img: 'https://static.comicvine.com/uploads/scale_small/6/67663/5996667-01.jpg',
+  publishingDate: 2017,
+  issues: []
+}
 
-    fetch(proxy + baseURL + '/issues/?api_key=' + apiKey, {mode: 'no-cors'})
-      .then(response => response.json())
-      .then(json => console.log(json))
+const manOfSteel = {
+  title: 'Man of Steel',
+  totalIssues: 6,
+  ownedIssues: 3,
+  status: 'Ongoing',
+  img: 'https://static.comicvine.com/uploads/scale_small/6/67663/6451280-01.jpg',
+  publishingDate: 2018,
+  issues: []
+}
+
+class CollectionSummary extends Component {
+  render() {
+    // define styles
+    const containerStyle = {
+      margin: '0 10px 10px',
+      overflow: 'auto'
+    }
+    const thumbStyle = {
+      float: 'left'
+    }
+    const imgStyle = {
+      display: 'block',
+      width: '100px'
+    }
+    const contentStyle = {
+      marginLeft: "110px",
+      lineHeigh: 0
+    }
+    return (
+      <div style={containerStyle}>
+        <div style={thumbStyle}>
+          <img style={imgStyle} alt="Collection Cover" src={this.props.data.img}></img>
+        </div>
+        <div style={contentStyle}>
+          <h3>{this.props.data.title}</h3>
+          <p>{this.props.data.ownedIssues}/{this.props.data.totalIssues} Issues</p>
+          <p>{this.props.data.status}</p>
+        </div>
+      </div>
+    )
   }
+}
 
+class Sidebar extends Component {
+  render() {
+    const sideStyle = {
+      borderRight: '1px solid white',
+      width: '300px',
+      height: '100%',
+      position: 'fixed',
+      zIndex: 1,
+      left: 0,
+      overflowX: 'hidden'
+    }
+
+    return (
+      <div style={sideStyle}>
+        <h2>Your Collections</h2>
+        <CollectionSummary data={misterMiracle}/>
+        <CollectionSummary data={manOfSteel}/>
+        <Search/>
+      </div>
+    )
+  }
+}
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text"/>
+        <button>Search</button>
+      </div>
+    )
+  }
+}
+
+class SearchResults extends Component {
+  render() {
+    const resultsStyle = {
+      marginLeft: '300px',
+      padding: '0 10px'
+    }
+
+    return (
+      <div style={resultsStyle}>
+        <h1>Search Results</h1>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Sidebar/>
+        <SearchResults/>
       </div>
     );
   }
